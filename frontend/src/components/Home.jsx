865255ef11c32d301dc22839733983f932b2af9b
Franck PERSONNE <fpersonne@meltone.com>
2025-07-23 16:51:59 +0200

    ajout du slider

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
+import Slider from './Slider';
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1 className="home-title">Bienvenue sur le Scraper de Chiens 🐶</h1>
       <p className="home-description">Cliquez sur "Races" pour voir les races de chien extraites depuis le web.</p>
+      <Slider />
     </div>
   );
 };
